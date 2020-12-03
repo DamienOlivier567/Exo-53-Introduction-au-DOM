706e739b5be4d90c72c136ea5eabc70e2db761a8
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-12-03 12:46:46 +0100

    Exo-53

---
+++ app.js
@@ -0,0 +1,35 @@
+let introductionElement = document.getElementById('paragraphe')
+console.log(introductionElement)
+introductionElement.style.color = 'red';
+introductionElement.style.width = '60rem';
+
+let element = document.getElementById('span')
+console.log(element)
+element.style.backgroundColor = 'white';
+element.dir = 'rtl';
+
+let introduction =  document.getElementById('animationii')
+console.log(introduction)
+introduction.style.animation = 'animation';
+introduction.style.animationDuration = '10s';
+
+let dernier = document.getElementById('dernier')
+console.log(dernier)
+dernier.innerText = "Voici mon nouveaux texte modifie avec JS";
+
+let lien = document.getElementById('lien')
+lien.textContent = "WWE";
+
+let lieni = document.getElementById('lieni')
+lieni.textContent = "UFC";
+
+let lienii = document.getElementById('lienii')
+lienii.textContent = "MMA";
+
+let lieniii = document.getElementById('lieniii')
+lieniii.textContent = "NWJP"
+
+let lieniv = document.getElementById('lieniv')
+lieniv.textContent = "TNA"
+
+
